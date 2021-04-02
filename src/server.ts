e33b05a8ff7a8f0ac1b16a7dc589691ea1bbe755
Ádám András <adm.andras@gmail.com>
2021-04-02 18:06:18 +0300

    Create simple express setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
+// Create Express app
 const app = express();
 
-const port = 5000;
+// Set port, default is 5000
+const port = process.env.PORT || 5000;
 
+// Set up BodyParser and CORS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.options("*", cors());
+
+// Test API
 app.get('/api', (req, res) => {
   res.status(200).send('API is running.');
 });
 
+// Start listening on port
 app.listen(port, () => console.log(`Forma API service listening on port ${port}.`));
