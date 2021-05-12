b773860d5c4cee9e9c42f1dd84457ad2541306b1
Ádám András <adm.andras@gmail.com>
2021-05-12 16:22:10 +0300

    Create forbidden exception

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,3 +1,4 @@
 export { HttpException } from './HttpException';
 export { NotFoundException } from './NotFoundException';
 export { ValidationException } from './ValidationException';
+export { ForbiddenException } from './ForbiddenException';
