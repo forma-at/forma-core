1153559d5d2ecf1cb7ad0f63a1d50f7f4c929925
Ádám András <adm.andras@gmail.com>
2021-05-13 19:12:18 +0300

    School name cannot be left empty now

--- src/services/schoolService.ts
+++ src/services/schoolService.ts
@@ -1,6 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { schoolRepository } from '../repositories';
 import { School, Address } from '../models';
+import { ValidationException } from '../exceptions';
 
 class SchoolService {
 
@@ -11,27 +12,35 @@ class SchoolService {
 
   // Create a new school
   async createSchool(name: string, description: string, address: Address) {
-    return schoolRepository.create({
-      id: uuid(),
-      name,
-      description,
-      address,
-    });
+    if (name.length < 1) {
+      throw new ValidationException('The school name cannot be left empty.');
+    } else {
+      return schoolRepository.create({
+        id: uuid(),
+        name,
+        description,
+        address,
+      });
+    }
   }
 
   // Update an existing school
   async updateSchool(school: School, name?: string, description?: string, address?: Address) {
-    return schoolRepository.update({ id: school.id }, {
-      name: name ?? school.name,
-      description: description ?? school.description,
-      address: {
-        street: address.street ?? school.address.street,
-        city: address.city ?? school.address.city,
-        zip: address.zip ?? school.address.zip,
-        state: address.state ?? school.address.state,
-        country: address.street ?? school.address.country,
-      },
-    });
+    if (name.length < 1) {
+      throw new ValidationException('The school name cannot be left empty.');
+    } else {
+      return schoolRepository.update({ id: school.id }, {
+        name: name ?? school.name,
+        description: description ?? school.description,
+        address: {
+          street: address.street ?? school.address.street,
+          city: address.city ?? school.address.city,
+          zip: address.zip ?? school.address.zip,
+          state: address.state ?? school.address.state,
+          country: address.street ?? school.address.country,
+        },
+      });
+    }
   }
 
 }
