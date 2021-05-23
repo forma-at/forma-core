d907b9d333a4c28b228cefb771bb634c6b2fa8a0
Ádám András <adm.andras@gmail.com>
2021-05-23 10:49:08 +0300

    Add teacher status consts

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,10 +1,19 @@
-type TeacherStatus = 'active' | 'pending';
+import { SanitizedUser } from './User';
+
+export const TEACHER_STATUS = {
+  PENDING: 'PENDING' as const,
+  ACTIVE: 'ACTIVE' as const,
+  REJECTED: 'REJECTED' as const,
+};
+
+export type TeacherStatus = keyof typeof TEACHER_STATUS;
 
 export class Teacher {
 
   userId: string;
   schoolId: string;
   status: TeacherStatus;
+  details?: SanitizedUser;
   createdAt: number;
   updatedAt: number;
 
