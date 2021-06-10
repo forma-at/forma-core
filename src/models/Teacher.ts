43d7a52464dc9b217314775c59058f57d673823c
András Ádám <adm.andras@gmail.com>
2021-06-10 19:35:08 +0300

    Add timestamps to teacher class

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -4,6 +4,8 @@ export class Teacher {
   id: string;
   userId: string;
   skills: Skill[];
+  createdAt: number;
+  updatedAt: number;
 
   constructor(teacher: Teacher) {
     Object.assign(this, teacher);
